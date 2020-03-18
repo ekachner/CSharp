f7840f2dbc82675b934acc55bcad1df1a66c6917
jwelchman <j.welchman@yahoo.com>
2020-03-18 09:40:06 -0600

    Justin C# Potato Challenge

---
+++ CSharp/Lab1/JustinWelchman/Week12Lab-PotatoGrading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Week12Lab_PotatoGrading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The following potato (looking at you, Brett) weight grades are given below:");
+            Console.WriteLine("Less than 200 gms – Grade X");
+            Console.WriteLine("Between 200 and 400 gms – Grade A");
+            Console.WriteLine("Between 400 and 800 gms – Grade B");
+            Console.WriteLine("Above 800 gms – Grade Z");
+            Console.WriteLine("Document results:");
+            string PotatoInput = Console.ReadLine();
+            int Potato = int.Parse(PotatoInput);
+
+            if (Potato <= 200)
+            {
+                Console.WriteLine("This potato weighs " + PotatoInput + "gms, and is therefore Grade X.");
+            }
+        }
+    }
+}
---
+++ CSharp/Lab1/JustinWelchman/Week12Lab-PotatoGrading/Week12Lab-PotatoGrading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Week12Lab_PotatoGrading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The following potato (looking at you, Brett) weight grades are given below:");
+            Console.WriteLine("Less than 200 gms – Grade X");
+            Console.WriteLine("Between 200 and 400 gms – Grade A");
+            Console.WriteLine("Between 400 and 800 gms – Grade B");
+            Console.WriteLine("Above 800 gms – Grade Z");
+            Console.WriteLine("Document results:");
+            string PotatoInput = Console.ReadLine();
+            int Potato = int.Parse(PotatoInput);
+
+            if (Potato < 200)
+            {
+                Console.WriteLine("This potato weighs " + PotatoInput + " gms, and is therefore Grade X.");
+            } else if (Potato >= 200 && Potato <= 400)
+            {
+                Console.WriteLine("This potato weighs " + PotatoInput + " gms, and is therefore Grade A.");
+            } else if (Potato >= 400 && Potato <= 800)
+            {
+                Console.WriteLine("This potato weighs " + PotatoInput + " gms, and is therefore Grade B.");
+            } else if (Potato > 800)
+            {
+                Console.WriteLine("This potato weighs " + PotatoInput + " gms, and is therefore Grade Z.");
+            } else
+            {
+                Console.WriteLine("ERROR: Insufficient Value.");
+            }
+        }
+    }
+}
