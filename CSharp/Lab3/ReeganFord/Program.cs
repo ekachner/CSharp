d82c50d637e6dbf7b7d76a002fd9c936d4521ba3
reeganford <rmford1695@gmail.com>
2020-03-26 15:42:29 -0600

    Reegan C# Lab3

---
+++ CSharp/Lab3/ReeganFord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cinema_Entries_Arrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
