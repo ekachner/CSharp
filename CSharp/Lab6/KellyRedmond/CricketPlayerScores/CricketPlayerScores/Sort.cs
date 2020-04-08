7ea5a9b7b7a284e7f1d1936e80f646e3b7259574
kredmond <kelsu02@gmail.com>
2020-04-07 22:08:42 -0600

    Kelly Redmond C# Lab6

---
+++ CSharp/Lab6/KellyRedmond/CricketPlayerScores/CricketPlayerScores/Sort.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace CricketPlayerScores
+{
+    public class Sort
+    {
+        public static void PickSort(Player[] players)
+        {
+            string sortAgain = "Y";
+
+            while (sortAgain == "Y")
+            {
+                int userSort = Methods.IsNumber("Roster order options" +
+                "\n\t1. Player Score High to Low\n\t2. Player Score Low to High" +
+                "\n\t3. Player Name A to Z\n\t4. Player Name Z to A\n", 1, 4, "selection");
+
+                switch(userSort)
+                {
+                    case 1:
+                        ScoreHighLow(players);
+                        break;
+
+                    case 2:
+                        ScoreLowHigh(players);
+                        break;
+
+                    case 3:
+                        AtoZ(players);
+                        break;
+
+                    case 4:
+                        ZtoA(players);
+                        break;
+
+                    default:
+                        Console.WriteLine("Entry not accepted, please restart program");
+                        break;
+                }
+
+                Team.PrintRoster(players);
+
+                sortAgain = Methods.StringInput("Would you like to sort another way? ( Y / N ): ", "Y", "N");
+
+                if (sortAgain == "Y")
+                {
+                    Console.Clear();
+                }
+            }
+        }
+
+        public static void ScoreHighLow(Player[] players)
+        {
+            Player temp;
+            bool trade = true;
+
+            for (int i = 0; (i <= (players.Length - 2)) && trade; i++)
+            {
+                trade = false;
+                for (int j = 0; j <= (players.Length - 2); j++)
+                {
+                    if(players[j + 1].Score > players[j].Score)
+                    {
+                        temp = players[j];
+                        players[j] = players[j + 1];
+                        players[j + 1] = temp;
+                        trade = true;
+                    }
+                }
+            }
+        }
+
+        public static void ScoreLowHigh(Player[] players)
+        {
+            ScoreHighLow(players);
+            Array.Reverse(players);
+        }
+
+        public static void AtoZ(Player[] players)
+        {
+            string[] arrKeys = new string[players.Length];
+            string[] arrValues = new string[players.Length];
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                arrKeys[i] = players[i].Name;
+                arrValues[i] = players[i].Score.ToString();
+            }
+
+            Array.Clear(players, 0, players.Length);
+
+            Array.Sort(arrKeys, arrValues);
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i] = new Player(arrKeys[i], int.Parse(arrValues[i]));
+            }
+        }
+
+        public static void ZtoA(Player[] players)
+        {
+            Player temp;
+            bool trade = true;
+
+            for (int i = 0; (i <= (players.Length - 2)) && trade; i++)
+            {
+                trade = false;
+                for (int j = 0; j <= (players.Length - 2); j++)
+                {
+                    if (players[j + 1].Name.CompareTo(players[j].Name) > 0 )
+                    {
+                        temp = players[j];
+                        players[j] = players[j + 1];
+                        players[j + 1] = temp;
+                        trade = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp/Lab6/KellyRedmond/CricketPlayerScores/CricketPlayerScores/Team.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace CricketPlayerScores
+{
+    public class Team
+    {
+        public const int NUMBEROFPLAYERS = 11;
+
+        public static Player[] PlayerEntry()
+        {
+            //int numPlayers = Methods.IsNumber("Enter the number of players: ", 0, NUMBEROFPLAYERS);
+            //Player[] players = new Player[numPlayers];
+
+            Player[] players = new Player[NUMBEROFPLAYERS];
+
+            int count = 1;
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                string playerCorrect = "N";
+
+                while (playerCorrect == "N")
+                {
+                    Console.WriteLine($"Player {count}");
+                    string playerName = Player.EnterName();
+                    int playerScore = Player.EnterScore(playerName);
+
+                    Console.Clear();
+
+                    Console.WriteLine($"Player {count}");
+                    playerCorrect = Methods.StringInput($"Name: {playerName}\nScore: {playerScore}\nIs this correct? ( Y / N ): ", "Y", "N");
+
+                    if (playerCorrect == "Y")
+                    {
+                        players[i] = new Player(playerName, playerScore);
+                        Console.WriteLine("Player added");
+                    }
+
+                    Console.Clear();
+                }
+
+                count++;
+            }
+
+            PrintRoster(players);
+
+            return players;
+        }
+
+        public static void PrintRoster(Player[] players)
+        {
+            Console.WriteLine("Team Roster - Average Score: {0}\nName\tScore", PrintAverageScore(players));
+            foreach (var player in players)
+            {
+                Console.WriteLine($"{player.Name}\t{Player.IsDuck(player.Score)}");
+            }
+            Console.WriteLine();
+        }
+
+        public static int PrintAverageScore(Player[] players)
+        {
+            int sum = 0;
+
+            foreach (var player in players)
+            {
+                sum += player.Score;
+            }
+
+            int average = sum / players.Length;
+
+            return average;
+        }
+    }
+}
