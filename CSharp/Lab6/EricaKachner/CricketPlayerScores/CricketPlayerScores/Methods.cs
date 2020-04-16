cc7b9bac242933621b08997738b958030b2d1a3f
ekachner <ekibam@gmail.com>
2020-04-16 16:59:32 -0600

    separated supplemental methods into new class Methods

--- CSharp/Lab6/EricaKachner/CricketPlayerScores/CricketPlayerScores/Methods.cs
+++ CSharp/Lab6/EricaKachner/CricketPlayerScores/CricketPlayerScores/Methods.cs
@@ -2,31 +2,63 @@
 
 namespace CricketPlayerScores
 {
-    class Methods
+     public class Methods
     {
-        string[] players = new string[11]; /*{ Ricky, Allan, Bobby, Dillon, Trevor, Bryce, Donovan, Asher, Marvin, Tommy, Billy };*/
-        int[] scores = new int[11];
 
-        static string ReadString(string prompt)
+        public static string EnterPlayerName()
+        {
+            string name = Methods.ReadString("Please enter a player's name: ");
+            return name;
+        }
+
+
+
+        public static string ReadString(string prompt)
         {
             string result;
             do
             {
                 Console.Write(prompt);
                 result = Console.ReadLine();
+                if (result == "")
+                {
+                    Console.WriteLine("Please enter the player's name: ");
+                }
             } while (result == "");
             return result;
         }
 
-        static int ReadInteger(string prompt, int min, int max)
+
+
+        public static int ReadInteger(string prompt, int min, int max)
         {
-            int result;
+            int result = min - 1;
             do
             {
-                string intString = ReadString(prompt);
-                result = int.Parse(intString);
+                try
+                {
+                    string intString = ReadString(prompt);
+                    result = int.Parse(intString);
+                    if (result < min || result > max)
+                    {
+                        Console.WriteLine($"Please list a score between {min} and {max}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + (e.Message) + $" Please enter a valid number between {min} and {max}");
+                }
             } while ((result < min) || (result > max));
             return result;
+        }
+
+
+
+        public static int EnterScore(int min, int max)
+        {
+
+            int score = ReadInteger($"Please enter his score: ", min, max);
+            return score;
         }
 
     }
@@ -36,33 +68,3 @@ namespace CricketPlayerScores
 
 
 
-
-
-
-//* Create 11 players -  Array[name, score]
-
-//* Input Score(prompt = enter score for batsmen name, min = 0 or “duck”, max = 500).  Anything outside this must be rejected
-
-//* Average Score  = total innings score / 11 
-
-
-//* Create a structure to store the score information about each player (refer to SharePoint lecture to store scores in each element of the array.) 
-
-//* Print cricket scores in order from largest to smallest (Bubble Sort)
-
-//* Then create method to sort by names of players. 
-//(Use CompareTo method). 
-//string a = “Fred”;
-//string b = “Jim”;
-//if(a.CompareTo(b) < 0)
-//{
-//    Console.WriteLine($“{ a} is before { b }”);
-//}
-//else
-//{
-//    Console.WriteLine($”{ b} is before { a }”);
-//}
-
-
-//* Create prompt that asks if you want to sort by name or by scores.
-//(Do you want to sort by name or by score? (Enter N or S): )
--- CSharp/Lab6/EricaKachner/CricketPlayerScores/CricketPlayerScores/Program.cs
+++ CSharp/Lab6/EricaKachner/CricketPlayerScores/CricketPlayerScores/Program.cs
@@ -5,31 +5,17 @@ namespace CricketPlayerScores
     class Program
     {
 
-
         public static void Main(string[] args)
         {
-            Player[] players = new Player[3];
-            
+            Player[] players = new Player[3];           
 
             EnterTeamStats(players);
-
             string sortOption = ChooseBetween("How do you wish to sort your list? By the names of the players or by their scores (Enter: N or S): ", "N", "S");
-
-
-            
             PrintPlayers(players, sortOption);
-
-
-
         }
 
 
-
-
-        
-
-
-
+        //creation of new "type" to make array of two type values
         struct Player
         {
             public string Name;
@@ -37,75 +23,21 @@ namespace CricketPlayerScores
         }
 
 
+
+        //cycles through each Player array to get a name and score for that player
         static void EnterTeamStats(Player[] players)
         {
             for (int i = 0; i < players.Length; i++)
             {
-                string name = EnterPlayerName();
-                int score = EnterScore(0, 500);
+                string name = Methods.EnterPlayerName();
+                int score = Methods.EnterScore(0, 500);
                 players[i] = new Player { Name = name, Score = score };
             }
         }
 
 
 
-        static string EnterPlayerName()
-        {
-            string name = ReadString("Please enter a player's name: ");
-            return name;
-        }
-
-
-        static string ReadString(string prompt)
-        {
-            string result;
-            do
-            {
-                Console.Write(prompt);
-                result = Console.ReadLine();
-                if(result == "")
-                {
-                    Console.WriteLine("Please enter the player's name: ");
-                }
-            } while (result == "");
-            return result;
-        }
-
-
-
-        static int EnterScore(int min, int max)
-        {
-
-            int score = ReadInteger($"Please enter his score: ", min, max);
-            return score;
-        }
-
-
-
-        static int ReadInteger(string prompt, int min, int max)
-        {
-            int result = min - 1;
-            do
-            {
-                try
-                {
-                    string intString = ReadString(prompt);
-                    result = int.Parse(intString);
-                    if(result < min || result > max)
-                    {
-                        Console.WriteLine($"Please list a score between {min} and {max}");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error: " + (e.Message) + $" Please enter a valid number between {min} and {max}");
-                }
-            } while ((result < min) || (result > max));
-            return result;
-        }
-
-
-
+        //asks user to choose between two values
         static string ChooseBetween(string prompt, string option1, string option2)
         {
             do
@@ -125,92 +57,32 @@ namespace CricketPlayerScores
 
 
 
+        //Prints players name and corresponding scores using the answer they chose from ChooseBetween()
         static void PrintPlayers(Player[] players, string chooseBetweenOption)
-        {
-            if(chooseBetweenOption == "N")
+        {            
+            if (chooseBetweenOption == "N")
             {
                 SortByName(players);
 
-                for (int i = 0; i < players.Length; i++)
-                {
-                    Console.WriteLine($"{players[i].Name} scored {players[i].Score} points.");
+                foreach(Player p in players)
+            {
+                    Console.WriteLine($"{p.Name} scored {p.Score} points");
                 }
             }
 
-            if(chooseBetweenOption == "S")
+            if (chooseBetweenOption == "S")
             {
                 SortByScores(players);
 
-                for (int i = 0; i < players.Length; i++)
+                foreach (Player p in players)
                 {
-                    Console.WriteLine($"{players[i].Name} scored {players[i].Score} points.");
+                    Console.WriteLine($"{p.Name} scored {p.Score} points");
                 }
             }
-
-            //foreach(Player p in players)
-            //{
-            //    Console.WriteLine($"{p.Name} scored {p.Score} points");
-            //}
         }
 
 
-
-
-
-
-
-        //static int[] EnterScore(int min, int max, string[] players, int[] scores)
-        //{
-        //    int result;
-
-        //    for (int i = 0; i < players.Length; i++)
-        //    {
-        //        result = ReadInteger($"Please enter a score for player {players[i]}", min, max);
-        //        scores[i] = result;
-        //        Console.WriteLine($"Player {players[i]} scored: {scores[i]}");
-        //    }
-        //    return scores;
-        //}
-
-
-        //static void PrintPlayers(string[] players, int[] scores)
-        //{
-        //    for (int i = 0; i < players.Length; i++)
-        //    {                
-        //            Console.WriteLine($"Player {i + 1}: " + players[i] + "\nScore: " + scores[i] + "\n");                
-        //    }
-        //}
-
-
-        
-
-
-
-        //BUBBLE SWAP: sorts scores highest to lowest
-        static void SortByScores(Player[] players)
-        {
-            bool doneSwapping;
-
-            do
-            {
-                doneSwapping = false;
-                for (int i = 0; i < players.Length - 1; i++)
-                {
-                    if (players[i].Score < players[i + 1].Score)
-                    {
-                        Player temp = players[i];
-                        players[i] = players[i + 1];
-                        players[i + 1] = temp;
-                        doneSwapping = true;
-                    }
-                }
-            } while (doneSwapping);
-        }
-
-
-
-
-
+        //sorts names alphabetically using character comparison method, inequality operators against "0" and BUBBLE SWAP
         static void SortByName(Player[] players)
         {
             bool doneSwapping;
@@ -231,40 +103,31 @@ namespace CricketPlayerScores
         }
 
 
-        //        //* Then create method to sort by names of players. 
-        //        //(Use CompareTo method). 
-        //        string a = “Fred”;
-        //        string b = “Jim”;
-        //        if(a.CompareTo(b) < 0)
-        //        {
-        //            Console.WriteLine($“{ a} is before { b}”);
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine($”{ b} is before { a }”);
-        //        }
+        //sorts scores highest to lowest using integer inequality operators and BUBBLE SWAP
+        static void SortByScores(Player[] players)
+        {
+            bool doneSwapping;
 
+            do
+            {
+                doneSwapping = false;
+                for (int i = 0; i < players.Length - 1; i++)
+                {
+                    if (players[i].Score < players[i + 1].Score)
+                    {
+                        Player temp = players[i];
+                        players[i] = players[i + 1];
+                        players[i + 1] = temp;
+                        doneSwapping = true;
+                    }
+                }
+            } while (doneSwapping);
+        }
     }
 }
 
 
 
-
-
-
-//* Create 11 players -  Array[name, score]
-//two separate arrays. Use index value to link score to player?
-
-
-
-//* Input Score(prompt = enter score for batsmen name, min = 0 or “duck”, max = 500).  Anything outside this must be rejected
-
-//* Average Score  = total innings score / 11 
-
-
-//* Create a structure to store the score information about each player (refer to SharePoint lecture to store scores in each element of the array.) 
-
-//* Print cricket scores in order from largest to smallest (Bubble Sort)
 
 
 
