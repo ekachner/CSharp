102a891d18b4b27c99c29ee6c294a3208574cc0c
kredmond <kelsu02@gmail.com>
2020-04-28 00:09:11 -0600

    Kelly Redmond C# CrazyTimers

---
+++ CSharp/CrazyTimers/KellyRedmond/CrazyTimers/CrazyTimers/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace CrazyTimers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string calculateAgain = "Y";
+            
+            while (calculateAgain == "Y")
+            {
+                Welcome();
+                calculateAgain = Methods.StringChoice("\nWould you like to calculate a different time? ( Y / N ): ", "Y", "N");
+                Console.Clear();
+            }
+        }
+
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome to the Time Calculators");
+            CalculatorChoice();
+        }
+
+        static void CalculatorChoice()
+        {
+            Console.WriteLine("There are three time calculators to choose from:\n\t1. Countdown Timer (hours, minutes, seconds into seconds)\n\t2. Reverse Countdown Timer (seconds into hours, minutes, seconds)\n\t3. Seconds to Calculator (seconds from now until chosen future hour)");
+            int calcChoice = Methods.VerifyNumber("Which Calculator would you like to use? 1, 2, or 3: ", 1, 3);
+            Console.Clear();
+            switch(calcChoice)
+            {
+                case 1:
+                    CountdownTimer();
+                    break;
+
+                case 2:
+                    ReverseCountdown();
+                    break;
+
+                case 3:
+                    SecondsToDate();
+                    break;
+
+                default:
+                    Console.WriteLine("The timer has hit a snag, please restart.");
+                    break;
+            }
+        }
+
+        static void CountdownTimer()
+        {
+            Console.WriteLine("Countdown Timer Calculator by Kelly Redmond\nVersion 1.0");
+            Time userTimer = new Time(Time.GetHours(), Time.GetMinutes(), Time.GetSeconds());
+            userTimer.CalculateSeconds();
+        }
+
+        static void ReverseCountdown()
+        {
+            Console.WriteLine("Reverse Countdown Timer Calculator by Kelly Redmond\nVersion 1.0");
+            Time userTimer = new Time();
+            userTimer.CalculateRegTime();
+        }
+
+        static void SecondsToDate()
+        {
+            Console.WriteLine("Seconds to Calculator by Kelly Redmond\nVersion 1.0");
+            Time userTimer = new Time();
+            userTimer.SecondsTill();
+
+        }
+    }
+}
---
+++ CSharp/CrazyTimers/KellyRedmond/CrazyTimers/CrazyTimers/Time.cs
@@ -0,0 +1,112 @@
+﻿using System;
+namespace CrazyTimers
+{
+    public class Time
+    {
+        static int Hours;
+        static int Minutes;
+        static int Seconds;
+        static int Day;
+        static int Month;
+        static int Year;
+
+        public Time(int hours = 0, int minutes = 0, int seconds = 0)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+
+        public static int GetHours()
+        {
+            Hours = Methods.VerifyNumber("Enter the number of hours: ", 0, 23);
+            return Hours;
+        }
+
+        public static int GetMinutes()
+        {
+            Minutes = Methods.VerifyNumber("Enter the number of minutes: ", 0, 59);
+            return Minutes;
+        }
+
+        public static int GetSeconds()
+        {
+            Seconds = Methods.VerifyNumber("Enter the number of seconds: ", 0, 59);
+            return Seconds;
+        }
+
+        public static int GetHour()
+        {
+            Hours = Methods.VerifyNumber("Enter the hour: ", 0, 23);
+            return Hours;
+        }
+
+        public static int GetDay()
+        {
+            Day = Methods.VerifyNumber("Enter the day: ", 1, 31);
+            return Day;
+        }
+
+        public static int GetMonth()
+        {
+            Month = Methods.VerifyNumber("Enter the month: ", 1, 12);
+            return Month;
+        }
+
+        public static int GetYear()
+        {
+            Year = Methods.VerifyNumber("Enter the year: ", 2020, 2500);
+            return Year;
+        }
+
+        public void CalculateSeconds()
+        {
+            Minutes = (Hours * 60) + Minutes;
+            Seconds = (Minutes * 60) + Seconds;
+
+            Console.WriteLine($"The total number of seconds is: {Seconds:N0}");
+        }
+
+        public void CalculateRegTime()
+        {
+            Seconds = Methods.VerifyNumber("Enter the number of seconds: ", 0, 86399);
+            Hours = Seconds / 3600;
+            Seconds -= (Hours * 3600);
+            Minutes = Seconds / 60;
+            Seconds -= (Minutes * 60);
+
+            Console.WriteLine("This is equal to {0} hours, {1} minutes and {2} seconds", Hours, Minutes, Seconds);
+        }
+
+        public void GetDate()
+        {
+            GetHour();
+            GetDay();
+            GetMonth();
+            GetYear();
+            while (Day > DateTime.DaysInMonth(Year, Month))
+            {
+                Console.WriteLine($"Please select day between 1 and {DateTime.DaysInMonth(Year, Month)}.");
+                GetDay();
+            }
+        }
+
+        public void SecondsTill()
+        {
+            GetDate();
+            DateTime date1 = DateTime.Now;
+            DateTime date2 = new DateTime(year: Year, month: Month, day: Day, hour: Hours, minute: 0, second: 0);
+            while (date2 < DateTime.Now)
+            {
+                Console.WriteLine("You must enter a future date");
+                GetDate();
+                date2 = new DateTime(year: Year, month: Month, day: Day, hour: Hours, minute: 0, second: 0);
+            }
+
+            TimeSpan interval = date2 - date1;
+            Console.WriteLine($"The total number of seconds is: {interval.TotalSeconds:N0}");
+
+        }
+    }
+}
