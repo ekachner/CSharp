ae855b998aa941f1b7f271d68217c46ee17699c6
reeganford <rmford1695@gmail.com>
2020-04-29 10:32:29 -0600

    Reegan Ford C# DiceTester Lab

---
+++ CSharp/DiceTester/ReeganFord/Dice_Chucker/Dice_Chucker/Chucker.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace Dice_Chucker
+{
+    class Chucker
+    {
+        static void Main()
+        {
+            bool anotherRoll = true;
+            do
+            {
+                FairDice.Roll();
+                Console.WriteLine("Roll again? (Press Enter) ");
+                Inputs.PressEnter(anotherRoll);
+
+            } while (anotherRoll == true);
+
+            Console.Clear();
+            Console.WriteLine("Thanks for playing!");
+        }
+    }
+}
