2de35e2f6c1df175636e4d69ad6666c078c4d40b
ekachner <ekibam@gmail.com>
2020-05-01 07:57:09 -0600

    fixed something weird

--- CSharp/CrazyTimers/EricaKachner/CrazyTimers/CrazyTimers/CrazyT-Program.cs
+++ CSharp/CrazyTimers/EricaKachner/CrazyTimers/CrazyTimers/CrazyT-Program.cs
@@ -11,17 +11,11 @@ namespace CrazyTimers
             SecondsTillEvent();
             SecondsToDays();
             DaysToSeconds();
-<<<<<<< HEAD
+
         }
 
         
 
-=======
-        }
-
-        
-        
->>>>>>> e93fd626d08fd075fbaa9e2d281fb83dcc13fc63
         static void SecondsTillEvent()
         {
             string eventName = ReadString("Enter the name of your event: ");
@@ -58,17 +52,10 @@ namespace CrazyTimers
             DateTime currentDate = DateTime.Now;
             TimeSpan timeLeft = eventDate.Subtract(currentDate);           
             Console.WriteLine($"\nThere are {Math.Ceiling(timeLeft.TotalSeconds)} seconds till {eventName}");
-<<<<<<< HEAD
         }
 
 
 
-=======
-        }
-        
-
-
->>>>>>> e93fd626d08fd075fbaa9e2d281fb83dcc13fc63
         static void SecondsToDays()
         {
             int result = -1;
@@ -163,12 +150,11 @@ namespace CrazyTimers
             } while (result == "");
             return result;
         }
-<<<<<<< HEAD
-    }
-}
-=======
+
+
     }
 }
 
 
->>>>>>> e93fd626d08fd075fbaa9e2d281fb83dcc13fc63
+
+
